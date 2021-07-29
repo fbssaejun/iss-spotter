feb03f23c075f8900e1c28fa880677f73e7e7e27
Anthony Kim <fbssaejun@gmail.com>
2021-07-29 23:11:53 +0000

    created and exported fetchISSFloyOverTimes function that fetches ISS floyover times according to our coordinates

--- iss.js
+++ iss.js
@@ -15,33 +15,59 @@ const fetchMyIP = function(callback) {
     
     let IP = JSON.parse(body);
     callback(null, IP.ip);
-    
+
   });
 };
 
 
 
 const fetchCoordsByIP = function(ip, callback) {
-  request('https://freegeoip.app/json/shit', (error, response, body) => {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
     if (error) {
       callback(error, null);
       return;
     }
 
     if (response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
-      callback(Error(msg), null);
+      callback(Error(`Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}), null`));
       return;
     }
 
     let coords = JSON.parse(body);
     const {longitude, latitude} = coords;
-    callback(null, {longitude, latitude});
+    callback(null, {longitude, latitude});    
 
   });
 };
 
 
-module.exports = { fetchMyIP, fetchCoordsByIP};
+ const fetchISSFlyOverTimes = function(coords, callback) {
+
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    if(!(coords.latitude <= 90 && coords.latitude >=-90) || !(coords.latitude <=180 && coords.latitude >= -180)) {
+      callback(Error(`Coordinates are not valid`))
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+
+  });
+};
+
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
 
 
